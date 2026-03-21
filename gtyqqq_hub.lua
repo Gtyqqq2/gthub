5cbbed80ce07874cf86060d5adb6e970ea3196c1
Gtyqqq2 <94034245+Gtyqqq2@users.noreply.github.com>
2026-03-21 12:20:57 +0700

    Update gtyqqq_hub.lua

--- gtyqqq_hub.lua
+++ gtyqqq_hub.lua
@@ -381,7 +381,7 @@ end)
 --================ MONSTER PULL =================--
 local pullEnabled = false
 local pullDistance = 21
-local pullRange = 150
+local pullRange = 100
 
 local pullFrame = Instance.new("Frame", pageTeleport)
 pullFrame.Size = UDim2.new(0.9,0,0,40)
